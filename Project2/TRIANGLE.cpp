ab19258d39f40c75a7c74b2a223cd49261af373b
ththfql <145094239+ththfql@users.noreply.github.com>
2026-03-25 15:24:49 +0900

    lll

---
+++ Project2/TRIANGLE.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+
+//표준라이브럴리에서 가져와서 사용할 이름을 언급
+using std::cin;
+using std::cout;
+using std::endl;
+
+//* 삼각형 만들기
+int main(void) {
+
+	cout << "what size TRI ?";
+	int size;
+	cin >> size; //10
+	/*
+	*******
+	 *****
+	  ***
+	   *
+	 */
+	//행 출력
+	for (int i = 0; i < size; i++) {
+		//열 출력
+		for (int j = 0; j < size; j++) {
+				cout << " "; 
+		}
+		for (int j = 0; j < (2 * size) - 1 - (i * 2); j++) {
+			cout <<  "*";
+		}
+		// 행 끝 줄 바꿈
+		cout << endl;
+
+	}
+
+
+	return 0;
+}
